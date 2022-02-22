ccd61e172b66d01d3d9b75dc8a096703bfc78e55
Dahye Min <48623630+dhmin401@users.noreply.github.com>
2022-02-21 23:25:26 -0500

    Modified App.js
    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
 import NotesList from "./component/NotesList";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import produce from 'immer';
 import { nanoid } from 'nanoid';
 
 function App() {
-  const [notes, setNotes]= useState([{
-    id: nanoid(),
-    title: "title 1",
-    text: "note text 1 ",
-    date: "20-2-2022"
-  },
-  {
-    id: nanoid(),
-    title: "title 2",
-    text: "note text 2",
-    date: "20-2-2022"
-  }]);
+  const [notes, setNotes]= useState([]);
 
   const addNote = (title,text)=>{
     const date = new Date();
-    const newNote = {
-      id: nanoid(),
-      title: title,
-      text: text,
-      date: date.toLocaleDateString()
-    };
-    const newNotes = [...notes, newNote]; //reducer create new array
-    setNotes(newNotes);//this update it
-    console.log(text);
+    const nextState = produce(notes, draftState => {
+      draftState.push({ 
+        id: nanoid(),
+        title: title,
+        text: text,
+        date: date.toLocaleDateString() });
+    });
+
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('notes', JSON.stringify(nextState));
+    }
+    setNotes(nextState);
   }
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const getNotes = localStorage.getItem('notes');
+
+      if (getNotes !== '' && getNotes !== null) {
+        return setNotes(JSON.parse(getNotes));
+      }
+      return setNotes([]);
+    }
+  }, []);
+
   const deleteNote = (id) =>{
-   const newNotes = notes.filter((note)=>note.id !== id);
-   setNotes(newNotes);
+   
+   const nextState = produce(notes, draftState => {
+      const index = draftState.findIndex(note => note.id === id);
+      if (index !== -1) {
+        draftState.splice(index, 1);
+      }
+  });
+
+   if (typeof window !== 'undefined') {
+      localStorage.setItem('notes', JSON.stringify(nextState));
+    }
+
+    setNotes(nextState);
   }
   return (
     <div className="container">
